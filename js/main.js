67862b122030e212288b3c5d7d8d4f1045ec9abf
Joseph Job <35570114+Jobp0001@users.noreply.github.com>
2018-02-08 23:41:52 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+     
+
+
+var output, a, b, c;
+a = 3;
+b = 5;
+
+output = "var a = 3;" + "\n" +
+         "var b = 5;" + "\n" +
+         "var c;" + "\n" + 
+         "-----------" + "\n" +
+         "a + b = " + (a + b) + "\n" +
+         "a - b = " + (a - b) + "\n" +
+         "a * b = " + (a * b) + "\n" +
+         "a / b :" + (a / b) + "\n" +
+         "a % b :" + (a % b )+"\n" +
+         "a % b :" + (a % b )+"\n" +
+          "a += b:" + (a += b) + "\n" +
+          "a -= b:" + (a -= b) + "\n" +
+          "a *= b :" + (a *= b) + "\n"+ 
+          "a /= b :"+ (a /= b) + "\n" +
+          "a %= b :"+ (a %= b) +"\n"+
+          "a == b:" + (a == b) +"\n" +
+          "a != b :"+ (a != b) +"\n"+
+          "a > b :" + (a > b) +"\n" +
+          "a < b: " + (a < b) +"\n"+
+          "!a && !c:" + (!a && !c) + "\n" + 
+          "!a || !c:" + (!a || !c) + "\n" ;
+
+alert(output);      
+
+
+var first_name = "JOSEPH";
+var last_name = "JOB";
+var email = "josephjp94@gmail.com";
+var output;
+
+output = "My name is"+ "\n" +  first_name + last_name + "\n" + "You can contact me at your-" + email;
+alert(output); 
+
+
+
+
+var numbers = [
+    Math.floor(Math.random() * (200 - 100) + 100), 
+    Math.floor(Math.random() * (200 - 100) + 100), 
+    Math.floor(Math.random() * (200 - 100) + 100), 
+    Math.floor(Math.random() * (200 - 100) + 100), 
+    Math.floor(Math.random() * (200 - 100) + 100)
+];
+ var n1 = numbers[0];
+ var n2 = numbers[(numbers.length - 1)];
+ var sum = (n1 + n2);
+
+
+if (sum % 2 === 0){
+    alert (n1 +" + "+ n2+ " = " + sum +"\n"+ " This is  an even Number");
+} else {
+    alert(n1 +" + "+ n2 +" = " + sum +"\n" +" This is  an odd Number");
+}
+
